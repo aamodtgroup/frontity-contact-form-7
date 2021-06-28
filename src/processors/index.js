f37e72193ba5d78ae0a4bc8472da83a245519218
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2021-06-28 11:50:35 +0200

    Add select to form

--- src/processors/index.js
+++ src/processors/index.js
@@ -3,6 +3,7 @@ import { cf7Inputs } from "./cf7Inputs";
 import { cf7Textarea } from "./cf7Textarea";
 import { cf7HiddenInputs } from "./cf7HiddenInputs";
 import { cf7Span } from "./cf7Span";
+import { cf7Select } from "./cf7Select";
 
 export default [
     cf7Form,
@@ -10,4 +11,5 @@ export default [
     cf7Textarea,
     cf7HiddenInputs,
     cf7Span,
+    cf7Select
 ];
