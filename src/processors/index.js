fc649af207fb5b4baf108e871d9c9e131730bf53
Kasper Aamodt <kasper@aamodt.xyz>
2021-07-02 20:11:32 +0200

    Added dropdown

--- src/processors/index.js
+++ src/processors/index.js
@@ -2,12 +2,15 @@ import { cf7Form } from './cf7Form';
 import { cf7Inputs } from "./cf7Inputs";
 import { cf7Textarea } from "./cf7Textarea";
 import { cf7HiddenInputs } from "./cf7HiddenInputs";
+import { cf7Dropdown } from "./cf7Dropdown";
 import { cf7Span } from "./cf7Span";
+
 
 export default [
     cf7Form,
     cf7Inputs,
     cf7Textarea,
     cf7HiddenInputs,
-    cf7Span,
+    cf7Dropdown,
+    cf7Span
 ];
